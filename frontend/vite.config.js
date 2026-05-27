d4ff120ffa95579118af640b85091e0359d97f6d
Sundar Lingam R <sundarlingam.rajasekar@analytixhub.ai>
2026-05-27 11:17:01 +0530

    chore(frontend): update vite base for cloudflare

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,22 @@
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   base: "/MDQM/",
+// });
+
+
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
@@ -13,5 +32,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/MDQM/",
+  base: "/",
 });
